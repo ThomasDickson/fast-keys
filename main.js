6f9d79564a18603ee400068ff5d645f00c48a752
Tom Dickson <104199731+ThomasDickson@users.noreply.github.com>
2023-02-10 23:51:28 +1100

    randomise words
    added a new dictionary of 300 most common english words, 100 of which are randomly selected on every reset

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-const SENTENCE = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
-               "Donec pellentesque ante ut metus pretium pellentesque. " +
-               "Sed quis nunc et nisi consectetur elementum id in ligula. " +
-               "Nulla vulputate tristique odio nec tristique. Donec feugiat " +
-               "aliquam massa nec vulputate. Donec purus arcu, faucibus a metus " +
-               "id, lobortis dignissim lorem. Vivamus et iaculis eros. Cras nec " +
-               "nibh in augue vestibulum finibus quis malesuada nisi." + " Donec dapibus, massa sit amet ornare ultrices";
+const dictionary = ["is", "was", "are", "be", "have","had", "were", "can", "said", "use","do", "will", "would", "make", "like","has", "look", "write", "go", "see","could", "been", "call", "am", "find","did", "get", "come", "made", "may","take", "know", "live", "give", "think","say", "help", "tell", "follow", "came","want", "show", "set", "put", "does","must", "ask", "went", "read", "need","move", "try", "change", "play", "spell","found", "study", "learn", "should", "add","keep", "start", "thought", "saw", "turn","might", "close", "seem", "open", "begin","got", "run", "walk", "began", "grow","took", "carry", "hear", "stop", "miss","eat", "watch", "let", "cut", "talk","being", "leave", "word", "time", "number", "way", "people","water", "day", "part", "sound", "work","place", "year", "back", "thing", "name","sentence", "man", "line", "boy", "farm","end", "men", "land", "home", "hand","picture", "air", "animal", "house", "page","letter", "point", "mother", "answer", "America","world", "food", "country", "plant", "school", "father", "tree", "city", "earth", "eye", "head", "story", "example", "life", "paper","group", "children", "side", "feet", "car","mile", "night", "sea", "river", "state","book", "idea", "face", "Indian", "girl","mountain", "list", "song", "family","he", "a", "one", "all", "an","each", "other", "many", "some", "two","more", "long", "new", "little", "most","good", "great", "right", "mean", "old","any", "same", "three", "small", "another","large", "big", "even", "such", "different","kind", "still", "high", "every", "own","light", "left", "few", "next", "hard","both", "important", "white", "four", "second","enough", "above", "young", "of", "to", "in", "for", "on","with", "at", "from", "by", "about","into", "down", "over", "after", "through","between", "under", "along", "until", "without","you", "that", "it", "he", "his","they", "I", "this", "what", "we","your", "which", "she", "their", "them","these", "her", "him", "my", "who","its", "me", "our", "us", "something","those", "and", "as", "or", "but","if", "than", "because", "while", "it's", "don't"]
 
 const TIME_LIMIT = 30;
 
 const wordList = document.getElementById("word-list");
 const inputField = document.getElementById("input-field");
 const resetButton = document.getElementById("reset-button");
+
+let words = [];
 
 let timer = null;
 let timeElapsed = 0;
@@ -52,6 +48,10 @@ inputField.addEventListener("keyup", e => {
         wordArray[currentWord].classList.add("highlight");
     }
 }); 
+
+function getRandomWord() {
+    return dictionary[Math.floor(Math.random() * dictionary.length)];
+}
 
 function startTimer() {
     clearInterval(timer);
@@ -95,13 +95,19 @@ function unloadText() {
         wordList.removeChild(child);
         child = wordList.lastElementChild;
     }
+    words = [];
 }
 
 function loadText() {
-    SENTENCE.split(' ').forEach(word => {
+    for(var i = 0; i < 100; i++)
+    {
+        console.log(getRandomWord())
+        words.push(getRandomWord());
         let wordSpan = document.createElement('span');
-        wordSpan.innerText = word + ' ';
+        wordSpan.innerText = words[i] + ' ';
         wordList.appendChild(wordSpan);
-    });
+    }
+    
+    
     wordList.firstChild.classList.add("highlight");
 }
